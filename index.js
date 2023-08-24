38c0099b10e0198c58bbfb6b30d7c670695e5131
Baran <baranali6688@gmail.com>
2023-08-24 14:25:04 +0300

    Ready for Deploy!

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
+require("dotenv").config()
 
 
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://svg-web.pages.dev');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
-mongoose.connect('mongodb://127.0.0.1:27017/bbquotes')
+mongoose.connect(process.env.DB_URL)
     .then(() => {
         console.log("Connected to MongoDB Successfully")
     })
     .catch((er) => {
         console.log("ERROR", er)
     })
+
 
 
 
