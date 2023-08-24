bd3d2a3abd37a1dbab6af9684885d46f8d533e05
Baran <baranali6688@gmail.com>
2023-08-24 14:55:11 +0300

    FIXING URL #2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://breakingb.pages.dev/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://breakingb.pages.dev');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
